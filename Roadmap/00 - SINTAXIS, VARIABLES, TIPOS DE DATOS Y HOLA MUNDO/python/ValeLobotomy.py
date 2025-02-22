2748002b310a59491f066acd198690322cf73689
ValeLobotomy <valeriayanceyepes@gmail.com>
2025-02-21 21:18:47 -0500

    Create ValeLobotomy.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ValeLobotomy.py
@@ -0,0 +1,27 @@
+#https://www.python.org/
+
+"""
+Comentario
+"""
+
+'''
+Comentario
+'''
+
+mi_nombre = "Valeria"
+mi_mascota = "Gatito"
+MI_CONSTANTE = "Mi constante"
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False 
+my_string = "Cadenita de texto"
+my_other_string = "Mi otra cadenita de texto" 
+
+print("Hola!, estoy Python")
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+print(type(my_other_string))
+print(mi_nombre, mi_mascota, MI_CONSTANTE)
